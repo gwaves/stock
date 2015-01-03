e397bb4fd591c02be3a3f9b5ec8953fb5b5b7607
Liu Tao <tommyliu.ustc@gmail.com>
2015-01-03 15:15:08 +0800

    mod divide to calc aver

--- ths_min5.py
+++ ths_min5.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division #to divde int number in float
 import struct
 from ths_opt import ths_real, ths_time, ths_time_print
 
@@ -15,13 +16,15 @@ for i in range(0,rec_num):
 	f.seek(start_pos+i*rec_len)
 	str1=f.read(28)  #in echo record, only 28 info have meaning
 	date,start,high,low,end,money,vol=struct.unpack("7i",str1) #7 int
+	rmoney=ths_real(money)
+	rvol=ths_real(vol)
 	try:
-		aver=ths_real(money)/ths_real(vol)	#some data have 0 vol for exsample "tingpai"
+		aver=rmoney/rvol	#some data have 0 vol for exsample "tingpai"
 	except ZeroDivisionError:
-		print "vol is zero"
+	#	print "vol is zero"
 		aver = 0
 	fmt_str="#%7.7d, time:%s:\tprice:%5.2f-%5.2f\trange:%5.2f-%5.2f, vol:%11d(%15d), aver:%5.2f"
-	print fmt_str%(i,ths_time_print(date),ths_real(start),ths_real(end),ths_real(low),ths_real(high),ths_real(vol), ths_real(money),aver)
+	print fmt_str%(i,ths_time_print(date),ths_real(start),ths_real(end),ths_real(low),ths_real(high),rvol, rmoney,aver)
 
 
 print 'over'
