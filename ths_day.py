eda9b833a6add59273f2a3fc12301e2b5775e08b
Liu Tao <tommyliu.ustc@gmail.com>
2015-01-03 15:14:28 +0800

    modify divde method to calc average

--- ths_day.py
+++ ths_day.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+from __future__ import division 
 import struct
 from ths_opt import ths_real, ths_time, ths_time_print
-
+from pylab import *
 
 f=open('600030.day','rb')
 i=0
@@ -10,7 +11,8 @@ str1=f.read(10)		#header info, include rec_num etc.
 rec_num,start_pos,rec_len,col_num=struct.unpack("i3h",str1) #one int, 3 short
 print "record number is:",rec_num
 print "data start position:%d;\trecord lenth:%d;\tcolom number:%d" %(start_pos,rec_len,col_num)
-
+all_end = []
+all_date = []
 for i in range(0,rec_num):
 	f.seek(start_pos+i*rec_len)
 	str1=f.read(28)  #in echo record, only 28 info have meaning
@@ -18,10 +20,13 @@ for i in range(0,rec_num):
 	try:
 		aver=ths_real(money)/ths_real(vol)	#some data have 0 vol for exsample "tingpai"
 	except ZeroDivisionError:
-		print "vol is zero"
+	#	print "vol is zero"
 		aver = 0
-	fmt_str="#%7.7d, time:%d:\tprice:%5.2f-%5.2f\trange:%5.2f-%5.2f, vol:%11d(%15d), aver:%5.2f"
-	print fmt_str%(i,date,ths_real(start),ths_real(end),ths_real(low),ths_real(high),ths_real(vol), ths_real(money),aver)
+#	fmt_str="#%7.7d, time:%d:\tprice:%5.2f-%5.2f\trange:%5.2f-%5.2f, vol:%11d(%15d), aver:%5.2f"
+#	print fmt_str%(i,date,ths_real(start),ths_real(end),ths_real(low),ths_real(high),ths_real(vol), ths_real(money),aver)
+	all_end.append(ths_real(end))
+	all_date.append(date-20140000)
+x=np.linspace(1,rec_num,rec_num)
+plot(all_date,all_end)
+show()
 
-
-print 'over'
